5480f21e4342e013021a897e82185f5eacd004ac
Annie <anniezhao107@gmail.com>
2020-09-05 17:06:29 -0700

    basic nav mesh enemy follow

---
+++ 3D Practice/Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    private NavMeshAgent monster;
+    public GameObject player;
+    public float monsDistanceRun = 15.0f;    //When the mob should start chasing
+    private float distance;
+    private Vector3 dirToPlayer;
+    private Vector3 newPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        monster = GetComponent<NavMeshAgent>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(transform.position, player.transform.position);
+
+        //Run towards player
+        if (distance < monsDistanceRun)
+        {
+            dirToPlayer = transform.position - player.transform.position;
+            newPos = transform.position - dirToPlayer;
+            monster.SetDestination(newPos);
+        }
+    }
+}
---
+++ 3D Practice/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int tokenCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tokenCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Game over");
+        }
+        else if (other.gameObject.CompareTag("Token"))
+        {
+            tokenCount += 1;
+            Debug.Log("Token count: "+tokenCount);
+            Destroy(other.gameObject);
+        }
+    }
+}
