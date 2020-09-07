d873139a71f0131a5729f140fadead4187d9009e
Annie <anniezhao107@gmail.com>
2020-09-06 23:46:43 -0700

    directional light excluded from player camera

---
+++ 3D Practice/Assets/Scripts/ExcludeLights.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExcludeLights : MonoBehaviour
+{
+    public List<Light> Lights;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnPreCull()
+    {
+        foreach (Light light in Lights)
+        {
+            light.enabled = false;
+            Debug.Log("light disabled");
+        }
+    }
+
+    void OnPostRender()
+    {
+        foreach (Light light in Lights)
+        {
+            light.enabled = true;
+        }
+    }
+
+}
